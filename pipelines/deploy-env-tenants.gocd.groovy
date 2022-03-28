86f08578d3ebfc22f09c79a3b203920ee2932f2f
Neha Negi <neha_negi@external.mckinsey.com>
2022-03-28 09:41:56 +0530

    gocd scripts

---
+++ pipelines/deploy-env-tenants.gocd.groovy
@@ -0,0 +1,56 @@
+def allEnvs = ['qa-tenant5',
+        'qa12-tenant5'];
+
+GoCD.script {
+    pipelines {
+        allEnvs.each {  env ->
+            pipeline("$env") {
+                def targetenv = env.split('-')[0]
+                group = 'deploy'
+                environmentVariables = [
+                        'ENV': 'DEV',
+                        'ENV_SIZE': 'small',
+                        'CLIENT':'client1',
+                        'BUILD_VERSION':'',
+                        'SNAPSHOT_VERSION':'',
+                        'TENANT_ID': 'tenant5',
+                        'UPDATE_DATA_BAGS': 'true',
+                        'BERKSFILE_PATH':'Berksfile',
+                        'CHEF_RUBY':'/opt/chef/embedded/bin',
+                        'BERKS_UPDATE':'false',
+                        'CHEF_ENV': targetenv
+                ]
+                materials {
+                    git {
+                        branch = 'main'
+                        url = 'git@github.com:twnehanegi/gocd-poc.git'
+                        blacklist = ['**/*']
+                        name = 'material0'
+                    }
+                }
+                stages {
+                    stage('deploy') {
+                        approval { type = 'manual' }
+                        fetchMaterials = true
+                        artifactCleanupProhibited = true
+                        cleanWorkingDir = false
+                        jobs {
+                            job('deploy') {
+                                tasks {
+                                    exec {
+                                        runIf = 'passed'
+                                        commandLine = ["/bin/sh", "echo \"Your current working directory is:\""]
+                                    }
+                                }
+                                runInstanceCount = null
+                                timeout = 60
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
