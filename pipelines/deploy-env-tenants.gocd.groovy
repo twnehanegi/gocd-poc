c978fe9bb67d4fad83d256b780200f3ddc084df8
Neha Negi <neha_negi@external.mckinsey.com>
2022-03-29 23:01:37 +0530

    Revert "Updated git uri"
    This reverts commit d901e39055ca1e61d6b8c51e7444a3ae27735217.

--- pipelines/deploy-env-tenants.gocd.groovy
+++ pipelines/deploy-env-tenants.gocd.groovy
@@ -24,7 +24,7 @@ GoCD.script {
                 materials {
                     git {
                         branch = 'main'
-                        url = 'https://github.com/twnehanegi/gocd-poc.git'
+                        url = 'git@github.com:twnehanegi/gocd-poc.git'
                         blacklist = ['**/*']
                         name = 'material-'+ environment
                     }
